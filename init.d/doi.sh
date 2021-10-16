53c7b25d18abdf5f407babe62d99f0695516cb54
4tikhonov <4tikhonov@gmail.com>
2021-10-16 15:25:38 +0200

    Init.d folder with configuration scripts

---
+++ init.d/doi.sh
@@ -0,0 +1,16 @@
+echo "Setting up the settings" >> /tmp/status.log
+echo  "- Allow internal signup" >> /tmp/status.log
+SERVER=${DATAVERSE_URL}/api
+curl -X PUT -d yes "$SERVER/admin/settings/:AllowSignUp"
+curl -X PUT -d /dataverseuser.xhtml?editMode=CREATE "$SERVER/admin/settings/:SignUpUrl"
+
+curl -X PUT -d doi "$SERVER/admin/settings/:Protocol"
+curl -X PUT -d 10.5072 "$SERVER/admin/settings/:Authority"
+curl -X PUT -d "FK2/" "$SERVER/admin/settings/:Shoulder"
+curl -X PUT -d FAKE "$SERVER/admin/settings/:DoiProvider"
+curl -X PUT -d burrito $SERVER/admin/settings/BuiltinUsers.KEY
+curl -X PUT -d localhost-only $SERVER/admin/settings/:BlockedApiPolicy
+curl -X PUT -d 'native/http' $SERVER/admin/settings/:UploadMethods
+curl -X PUT -d solr:8983 "$SERVER/admin/settings/:SolrHostColonPort"
+echo
+
