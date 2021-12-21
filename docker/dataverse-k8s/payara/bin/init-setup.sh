e21685ef12bfeb6541979cfefdabc81f72d06c18
4tikhonov <4tikhonov@gmail.com>
2021-12-21 10:28:48 +0100

    Simple health check and init.d folder

--- docker/dataverse-k8s/payara/bin/init-setup.sh
+++ docker/dataverse-k8s/payara/bin/init-setup.sh
@@ -19,10 +19,8 @@ DV_SU_PASSWORD="admin"
 
 command -v jq >/dev/null 2>&1 || { echo >&2 '`jq` ("sed for JSON") is required, but not installed. Download the binary for your platform from http://stedolan.github.io/jq/ and make sure it is in your $PATH (/usr/bin/jq is fine) and executable with `sudo chmod +x /usr/bin/jq`. On Mac, you can install it with `brew install jq` if you use homebrew: http://brew.sh . Aborting.'; exit 1; }
 
-echo "deleting all data from Solr"  >> /tmp/status.log;
-curl http://solr:8983/solr/collection1/update/json?commit=true -H "Content-type: application/json" -X POST -d "{\"delete\": { \"query\":\"*:*\"}}" >> /tmp/status.log;
-
-SERVER=http://localhost:8080/api
+#SERVER=http://localhost:8080/api
+SERVER=${DATAVERSE_URL}/api
 
 # Everything + the kitchen sink, in a single script
 # - Setup the metadata blocks and controlled vocabulary
@@ -32,67 +30,54 @@ SERVER=http://localhost:8080/api
 # - Create admin user and root dataverse
 # - (optional) Setup optional users and dataverses
 
+# Check if Dataverse is online
+healthcheck="/tmp/healthcheck.log"
+curl http://localhost:8080/api/dataverses/root|grep "description" >> $healthcheck
+sleep 3
 
-echo "Setup the metadata blocks" >> /tmp/status.log
-bash ./setup-datasetfields.sh
+if [ -s $healthcheck ];
+then
+        echo "Dataverse exists. Skipping setup from scratch..."
+else
+	# Setup Dataverse if it's empty
+	echo "Setup Dataverse from scratch..." >> /tmp/status.log
+	cd bash ${HOME_DIR}/dvinstall
+	echo "Setup the metadata blocks" >> /tmp/status.log
+	bash ${HOME_DIR}/dvinstall/setup-datasetfields.sh
 
-echo "Setup the builtin roles" >> /tmp/status.log
-bash ./setup-builtin-roles.sh
+	echo "Setup the builtin roles" >> /tmp/status.log
+	bash ${HOME_DIR}/dvinstall/setup-builtin-roles.sh
 
-echo "Setup the authentication providers" >> /tmp/status.log
-bash ./setup-identity-providers.sh
+	echo "Setup the authentication providers" >> /tmp/status.log
+	bash ${HOME_DIR}/dvinstall/setup-identity-providers.sh
 
-
-
-echo "Setting up the settings" >> /tmp/status.log
-echo  "- Allow internal signup" >> /tmp/status.log
-curl -X PUT -d yes "$SERVER/admin/settings/:AllowSignUp"
-curl -X PUT -d /dataverseuser.xhtml?editMode=CREATE "$SERVER/admin/settings/:SignUpUrl"
-
-curl -X PUT -d doi "$SERVER/admin/settings/:Protocol"
-curl -X PUT -d 10.5072 "$SERVER/admin/settings/:Authority"
-curl -X PUT -d "FK2/" "$SERVER/admin/settings/:Shoulder"
-curl -X PUT -d FAKE "$SERVER/admin/settings/:DoiProvider"
-curl -X PUT -d burrito $SERVER/admin/settings/BuiltinUsers.KEY
-curl -X PUT -d localhost-only $SERVER/admin/settings/:BlockedApiPolicy
-curl -X PUT -d 'native/http' $SERVER/admin/settings/:UploadMethods
-curl -X PUT -d solr:8983 "$SERVER/admin/settings/:SolrHostColonPort"
-echo
+	bash ${HOME_DIR}/dvinstall/setup-all.sh
 
 
+	echo "Setting up the admin user (and as superuser)" >> /tmp/status.log
+	adminResp=$(curl -s -H "Content-type:application/json" -X POST -d @data/user-admin.json "$SERVER/builtin-users?password=$DV_SU_PASSWORD&key=burrito")
+	echo $adminResp
+	curl -X POST "$SERVER/admin/superuser/dataverseAdmin"
+	echo
 
-echo "Setting up the admin user (and as superuser)" >> /tmp/status.log
-adminResp=$(curl -s -H "Content-type:application/json" -X POST -d @data/user-admin.json "$SERVER/builtin-users?password=$DV_SU_PASSWORD&key=burrito")
-echo $adminResp
-curl -X POST "$SERVER/admin/superuser/dataverseAdmin"
-echo
-
-echo "Setting up the root dataverse" >> /tmp/status.log
-adminKey=$(echo $adminResp | jq .data.apiToken | tr -d \")
-curl -s -H "Content-type:application/json" -X POST -d @data/dv-root.json "$SERVER/dataverses/?key=$adminKey"
-echo
-echo "Set the metadata block for Root" >> /tmp/status.log
-curl -s -X POST -H "Content-type:application/json" -d "[\"citation\"]" $SERVER/dataverses/:root/metadatablocks/?key=$adminKey
-echo
-echo "Set the default facets for Root" >> /tmp/status.log
-curl -s -X POST -H "Content-type:application/json" -d "[\"authorName\",\"subject\",\"keywordValue\",\"dateOfDeposit\"]" $SERVER/dataverses/:root/facets/?key=$adminKey
-echo
-
-if [ "${CVM_SERVER_NAME}" ]; then
-    echo "Uploading ${CVM_SERVER_NAME} metadatablock" >> /tmp/status.log
-    curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @data/metadatablocks/cvmm.tsv -H "Content-type: text/tab-separated-values"
-
-    #curl -H "Content-Type: application/json" -X PUT --data-binary @data/cvm-setting.json "$SERVER/admin/settings/:CVMConf"
-    echo "Uploading cvm-setting.json" >> /tmp/status.log
-
+	echo "Setting up the root dataverse" >> /tmp/status.log
+	adminKey=$(echo $adminResp | jq .data.apiToken | tr -d \")
+	curl -s -H "Content-type:application/json" -X POST -d @data/dv-root.json "$SERVER/dataverses/?key=$adminKey"
+	echo
+	echo "Set the metadata block for Root" >> /tmp/status.log
+	curl -s -X POST -H "Content-type:application/json" -d "[\"citation\"]" $SERVER/dataverses/:root/metadatablocks/?key=$adminKey
+	echo
+	echo "Set the default facets for Root" >> /tmp/status.log
+	curl -s -X POST -H "Content-type:application/json" -d "[\"authorName\",\"subject\",\"keywordValue\",\"dateOfDeposit\"]" $SERVER/dataverses/:root/facets/?key=$adminKey
+	echo
 fi
 
-if [ "${CVM_CONFIG}" ]; then
-    echo "Dowload keywords configuration file from ${CVM_TSV_SOURCE}" >> /tmp/status.log;
-    wget -O ${HOME_DIR}/dvinstall/data/metadatablocks/keys_config.json ${CVM_CONFIG}
-    wget -O ${HOME_DIR}/dvinstall/data/metadatablocks/cvm.sql ${CVM_SQL}
-    curl -H "Content-Type: application/json" -X PUT --data-binary @${HOME_DIR}/dvinstall/data/metadatablocks/keys_config.json http://localhost:8080/api/admin/settings/:CVMConf
-    psql -U dvnuser dvndb -h postgres -f ${HOME_DIR}/dvinstall/data/metadatablocks/cvm.sql
+# Run scripts from init.d folder to finish configuration
+if [ "${INIT_SCRIPTS_FOLDER}" ]; then
+    for initscript in $INIT_SCRIPTS_FOLDER/*
+        do
+           bash "$initscript"
+        done
 fi
 
 # OPTIONAL USERS AND DATAVERSES
